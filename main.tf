00cc3e6e5f04b8da0044989b5568ffb056496a34
Satendra-rajaure <145276046+Satendra-rajaure@users.noreply.github.com>
2024-11-19 17:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 
 module "ec2_instance" {
-  source        = "./modules/ec2"
+  source        = "./module/ec2"
   ami_id        = var.ami_ids
   instance_type = var.ec2_instance_type
-  key_name      = "cicd"
+  key_name      = "test01"
   instance_name = var.instance_name
 }
 
 module "s3" {
-  source = "./modules/s3"
+  source = "./module/s3"
 }
 
 module "vpc" {
-  source   = "./modules/vpc"
+  source   = "./module/vpc"
   vpc_name = "my-vpc"
 }
 
 module "subnet" {
-  source                  = "./modules/subnet"
+  source                  = "./module/subnet"
   vpc_id                  = module.vpc.vpc_id
   subnet_name             = "my-subnet"
   subnet_cidr             = "10.0.1.0/24"
